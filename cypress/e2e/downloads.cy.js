31543bb35fa21214afcf885dccfeb3f35ba35b88
Samuel de la Cruz <sdelacru@redhat.com>
2025-09-04 19:16:39 +0200

    Added device tests, fleet tests, repository test and downloads

--- cypress/e2e/downloads.cy.js
+++ cypress/e2e/downloads.cy.js
@@ -1,6 +1,6 @@
 describe('Downloads Management', () => {
     it('Should download os flightctl cli', () => {
-        cy.login(`${Cypress.env('host')}`, `${Cypress.env('auth')}`, `${Cypress.en
+        cy.login(`${Cypress.env('host')}`, `${Cypress.env('auth')}`, `${Cypress.env('username')}`, `${Cypress.env('password')}`)
         cy.downloadFile(`${Cypress.env('os')}`, `${Cypress.env('arch')}`)
     })
 })
